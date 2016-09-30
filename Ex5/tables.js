f3d7ed04fca77563ccaaf23fbade96376c6d0fd8
Damilola Animashaun <oluwadamilola@dealdey.com>
2016-09-30 14:55:45 +0100

    Exercise 5 Redone

--- Ex5/tables.js
+++ Ex5/tables.js
@@ -1,82 +1,82 @@
 function Contact(name,email){
   var _contact = this;
-  this.Name = name;
-  this.Email = email;
-  this.HoldingRow = document.createElement("tr");
-  this.ColumnOne = makeACell();
-  this.ColumnTwo = makeACell();
-  this.ColumnThree = makeACell();
-  this.NameSpan = document.createElement("span");
-  this.EmailSpan = document.createElement("span");
-  this.NameField = document.createElement("input");
-  this.EmailField = document.createElement("input");
-  this.EmailField.type = "email";
-  this.EditButton = makeAButton("Edit");
-  this.DeleteButton = makeAButton("Delete");
-  this.SaveButton = makeAButton("Save");
-  this.SaveButton.addEventListener("click", function(){
-    SaveData(_contact);
+  this.name = name;
+  this.email = email;
+  this.holdingRow = document.createElement("tr");
+  this.columnOne = makeACell();
+  this.columnTwo = makeACell();
+  this.columnThree = makeACell();
+  this.nameSpan = document.createElement("span");
+  this.emailSpan = document.createElement("span");
+  this.nameField = document.createElement("input");
+  this.emailField = document.createElement("input");
+  this.emailField.type = "email";
+  this.editButton = makeAButton("Edit");
+  this.deleteButton = makeAButton("Delete");
+  this.saveButton = makeAButton("Save");
+  this.saveButton.addEventListener("click", function(){
+    _contact.saveData(_contact);
   } , false);
-  this.EditButton.addEventListener("click", function(){
+  this.editButton.addEventListener("click", function(){
     _contact.formify();
   } , false);
-  this.DeleteButton.addEventListener("click", function(){
+  this.deleteButton.addEventListener("click", function(){
     table.tBodies[0].deleteRow(_contact.index);
   } , false);
-  this.HoldingRow.appendChild(this.ColumnOne);
-  this.HoldingRow.appendChild(this.ColumnTwo);
-  this.HoldingRow.appendChild(this.ColumnThree);
+  this.holdingRow.appendChild(this.columnOne);
+  this.holdingRow.appendChild(this.columnTwo);
+  this.holdingRow.appendChild(this.columnThree);
   this.index = 0;
 }
 
 Contact.prototype.flushColumns = function(){
-  this.ColumnOne.innerHTML = '';
-  this.ColumnTwo.innerHTML = '';
-  this.ColumnThree.innerHTML = '';
+  this.columnOne.innerHTML = '';
+  this.columnTwo.innerHTML = '';
+  this.columnThree.innerHTML = '';
 };
 
 Contact.prototype.giveValues = function(){
-  this.NameField.value = this.Name;
-  this.EmailField.value = this.Email;
+  this.nameField.value = this.name;
+  this.emailField.value = this.email;
 };
 
 Contact.prototype.updateSpans = function(){
-  this.NameSpan.innerText = this.Name;
-  this.EmailSpan.innerText = this.Email;
+  this.nameSpan.innerText = this.name;
+  this.emailSpan.innerText = this.email;
 };
 
 Contact.prototype.display = function() {
   this.updateSpans();
   this.flushColumns();
-  this.ColumnOne.appendChild(this.NameSpan);
-  this.ColumnTwo.appendChild(this.EmailSpan);
-  this.ColumnThree.appendChild(this.EditButton);
-  this.ColumnThree.appendChild(this.DeleteButton);
+  this.columnOne.appendChild(this.nameSpan);
+  this.columnTwo.appendChild(this.emailSpan);
+  this.columnThree.appendChild(this.editButton);
+  this.columnThree.appendChild(this.deleteButton);
 };
 
 Contact.prototype.formify = function(){
   this.giveValues();
   this.flushColumns();
-  this.ColumnOne.appendChild(this.NameField);
-  this.ColumnTwo.appendChild(this.EmailField);
-  this.ColumnThree.appendChild(this.SaveButton);
+  this.columnOne.appendChild(this.nameField);
+  this.columnTwo.appendChild(this.emailField);
+  this.columnThree.appendChild(this.saveButton);
 };
 
 Contact.prototype.Unformify = function(){
-  this.Name = this.NameField.value;
-  this.Email = this.EmailField.value;
+  this.name = this.nameField.value;
+  this.email = this.emailField.value;
   this.display();
 };
 
-function SaveData(Contact) {
-  if (Contact.NameField.value.length < 1 || Contact.EmailField.value.length < 1 ) {
+Contact.prototype.saveData =  function () {
+  if (this.nameField.value.length < 1 || this.emailField.value.length < 1 ) {
     alert('Incomplete Details.');
-  } else if( !Contact.EmailField.checkValidity() ) {
+  } else if( !this.emailField.checkValidity() ) {
     alert('Please provide a valid email.');
   } else {
-    Contact.Unformify();
+    this.Unformify();
   }
-}
+};
 
 function makeAButton(text) {
   var button =  document.createElement("button");
@@ -94,14 +94,18 @@ function makeACell(value) {
   return td;
 }
 
-var table = document.getElementById('table');
-table.border = 1;
+function init(){
+  var table = document.getElementById('table');
+  table.border = 1;
 
-var addBtn = document.getElementById('addNewItem');
-addBtn.addEventListener("click",function() {
-  //this.disabled = true;
-  var contact = new Contact('','');
-  contact.formify();
-  table.tBodies[0].appendChild(contact.HoldingRow);
-  contact.index =  table.tBodies[0].rows.length - 1;
-},false);
+  var addBtn = document.getElementById('addNewItem');
+  addBtn.addEventListener("click",function() {
+    //this.disabled = true;
+    var contact = new Contact('','');
+    contact.formify();
+    table.tBodies[0].appendChild(contact.holdingRow);
+    contact.index =  table.tBodies[0].rows.length - 1;
+  },false);
+}
+
+init();
