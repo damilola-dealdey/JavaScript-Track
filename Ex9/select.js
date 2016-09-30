341e40997d37f0c2c07a619f1fe969d4815dff26
Damilola Animashaun <oluwadamilola@dealdey.com>
2016-09-30 15:25:14 +0100

    Exercise 9 Redone

--- Ex9/select.js
+++ Ex9/select.js
@@ -1,17 +1,26 @@
-function Select(SelectElemId, TransferToElemId , ButtonELemId ){
-  this.MyBox = document.getElementById(SelectElemId);
-  this.TransferToBox = document.getElementById(TransferToElemId);
-  this.MyButton = document.getElementById(ButtonELemId);
+function Select(selectElemId, transferToElemId , buttonELemId){
+  this.init(selectElemId, transferToElemId , buttonELemId);
+}
+
+Select.prototype.init = function(selectElemId, transferToElemId , buttonELemId) {
+  var dom = document;
+  this.myBox = dom.getElementById(selectElemId);
+  this.transferToBox = dom.getElementById(transferToElemId);
+  this.myButton = dom.getElementById(buttonELemId);
   var _select = this;
-  this.MyButton.addEventListener("click", function(){
-    var selectedIndex = _select.MyBox.selectedIndex;
-    if (selectedIndex > -1) {
-      var option = _select.MyBox.options[selectedIndex];
-      _select.MyBox.remove(selectedIndex);
-      _select.TransferToBox.add(option);
-    }
+  this.myButton.addEventListener("click", function(){
+    _select.transfer();
   } , false);
 }
 
-var firstBox = new Select('boxOne','boxTwo','addBtn');
-var secondBox = new Select('boxTwo','boxOne','rmvBtn');
+Select.prototype.transfer = function() {
+  var selectedIndex = this.myBox.selectedIndex;
+  if (selectedIndex > -1) {
+    var option = this.myBox.options[selectedIndex];
+    this.myBox.remove(selectedIndex);
+    this.transferToBox.add(option);
+  }
+}
+
+var firstBox = new Select('box-one','box-two','add-btn');
+var secondBox = new Select('box-two','box-one','rmv-btn');
