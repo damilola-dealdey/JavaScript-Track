ebfd8147c6901b56d03f9f36826e70999def7031
Damilola Animashaun <oluwadamilola@dealdey.com>
2016-10-05 09:54:15 +0100

    Exercise Two Redone

--- Ex2/toggle_button.js
+++ Ex2/toggle_button.js
@@ -7,24 +7,20 @@ ToggleButton.prototype.init = function() {
   var dom = document;
   var checkBtn = dom.getElementById('all-check');
   checkBtn.addEventListener("click",function() {
-      _this.toggleBoxes(true);
+    _this.toggleBoxes(true);
   },false);
 
   var uncheckBtn = dom.getElementById('all-uncheck');
   uncheckBtn.addEventListener("click",function() {
-      _this.toggleBoxes(false);
+    _this.toggleBoxes(false);
   },false);
-}
+};
 
 ToggleButton.prototype.toggleBoxes = function(status) {
   var boxes = document.querySelectorAll("input.checkbox");
-  for (var i = 0; i < boxes.length; i++) {
-    if (status) {
-      boxes[i].checked = true;
-    } else {
-      boxes[i].checked = false;
-    }
+  for (var i = 0; i < boxes.length; i++) {    
+      boxes[i].checked = status;
   }
-}
+};
 
 new ToggleButton();
